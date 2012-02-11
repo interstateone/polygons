07696e2a41fdb9540e3f0613d7e909a4e4dfda2a
Brandon Evans <brandon@brandonevans.ca>
2012-02-10 20:21:47 -0700

    Works better for intersects, removed set.clip() for now

--- raphael.polygons.js
+++ raphael.polygons.js
@@ -8,59 +8,12 @@
 // Based on Efficient Clipping of Arbitrary Polygons by Gunther Greiner and Kai Hormann
 // http://davis.wpi.edu/~matt/courses/clipping/
 // 
-// Gives the ability to find the intersection of two or more polygons as a new polygon.
-// They can be concave or convex, but self-intersecting may not work properly.
-// Uses "node" objects to represent vertices of polygons and their intersection points.
-// 
-// Utility Functions:
-// 	- doesIntersect
-// 		Takes two pairs of node objects representing line segments: no ------- n1, n2 ------- n3
-// 		If they intersect a node is returned, if they don't intersect then "false" is returned
-// 
-// 	- norm
-// 		Takes one pair of node objects representing one line segment
-// 		Returns the norm/length/magnitude of the line
-// 
-// 	- clipIntersect
-// 		Needs to be deprecated, not really needed
-// 
-// 	- containmentTest
-// 		Uses the even/odd rule to determine whether a point is inside another polygon
-// 		Used by markEntryPoints in order to determine whether an intersection point is an entry or exit
-// 		Takes a single node and a node array representing a polygon
-// 		Returns 0 for an outside point or 1 for an inside point
-// 
-// 	- markEntryPoints
-// 		Marks the intersection points in one polygon as being entry or exits for the edges of the other polygon
-// 		Uses containmentTest to determine the initial status, then alternates entry/exit
-// 		Takes two node arrays to compare, and a flag setting
-// 		Returns the updated node array with entry/exit statuses
-// 
-// 	- Raphael.el.toNodes
-// 		Function that converts an element's path to the node array that's needed for clipping
-// 		Also makes sure that the nodes are in CW order
-// 		Returns a node array in CW order
-// 
-// The Good Stuff:
-// 	- Raphael.fn.clip
-// 		The general clipping function
-// 		Takes two node arrays representing subject and clip, but the order is not important
-// 		Returns the node array of the intersection or false if there isn't an intersection
-// 
-// 	- Raphael.st.clip
-// 		Takes advantage of the general clip function with sets
-// 		Useful if there are more than two polygons you need to clip, this will find the total intersection
-// 
 // ************************************************************************************************
 
-// Node object
-// Used for each vertex of a polygon and the intersects
-function node (x, y, intersect, alpha) {
+function Node(x, y, intersect) {
 	this.x = x;
 	this.y = y;
-
-	this.intersect = intersect;
-	this.alpha = alpha;
+	this.intersect = (intersect) ? intersect : false;
 	this.entry = false;
 	this.visited = false;
 
@@ -70,130 +23,111 @@ function node (x, y, intersect, alpha) {
 		
 		// Get vectors for the line segment and from this node to n0
 		// Ratio of dot prodcuts gives the projection of one onto the other
-
-		var v0    = new node(n1.x - n0.x, n1.y - n0.y),
-			vthis = new node(this.x - n0.x, this.y - n0.y),
+		var v0    = new Node(n1.x - n0.x, n1.y - n0.y),
+			vthis = new Node(this.x - n0.x, this.y - n0.y),
 			ratio,
 			denom = v0.x * v0.x + v0.y * v0.y;
 		
 		// First line segment has zero length, are these equal points?
-		if (denom == 0.0) {
+		if (denom === 0.0) {
 			return (this.x == n0.x && this.y == n0.y);
 		} else {
 			ratio = (v0.x * vthis.x + v0.y * vthis.y ) / denom;
 			return ((ratio >= 0.0) && (ratio <= 1.0));
 		}
-	}
+	};
 
 	this.toString = function() {
 		return this.x + "," + this.y;
-	}
+	};
 }
+
+// Node object
+// Used for each vertex of a polygon and the intersects
 
 // Finds if two line segments intersect
 // Takes two pairs of nodes
 // Returns: the intersect node or false
 // http://stackoverflow.com/a/1968345/1082395
-doesIntersect = function (n0, n1, n2, n3) {
+var doesIntersect = function(n0, n1, n2, n3) {
 
 	var denom = (n3.y - n2.y) * (n1.x - n0.x) - (n3.x - n2.x) * (n1.y - n0.y);
 
     // Parallel
     if (denom === 0) {
-    	return false;
+		return false;
     }
 
     var ua = ((n3.x - n2.x) * (n0.y - n2.y) - (n3.y - n2.y) * (n0.x - n2.x)) / denom,
-    	ub = ((n1.x - n0.x) * (n0.y - n2.y) - (n1.y - n0.y) * (n0.x - n2.x)) / denom,
-    	x = n0.x + ua * (n1.x - n0.x),
-    	y = n0.y + ua * (n1.y - n0.y),
-    	result = new node(x, y, true, 0);
+		ub = ((n1.x - n0.x) * (n0.y - n2.y) - (n1.y - n0.y) * (n0.x - n2.x)) / denom,
+		x = n0.x + ua * (n1.x - n0.x),
+		y = n0.y + ua * (n1.y - n0.y),
+		result = new Node(x, y, true);
 
     if (0 <= ua && ua <= 1 && 0 <= ub && ub <= 1) {
         // Collision detected
         return result;
     } else {
 		// No collision
-		return false; 	
+		return false;
     }
-}
+};
 
 // Returns the norm (length) of a line vector
-norm = function(n0, n1) {
+var norm = function(n0, n1) {
 	return Math.sqrt(((n1.x - n0.x) * (n1.x - n0.x)) + ((n1.y - n0.y) * (n1.y - n0.y)));
-}
-
-// Line intersect function specifically for clipping polygons
-// Takes two line segments of node objects L1(n0, n1), L2(n2, n3)
-// Returns the point and alpha values as an object if they intersect, otherwise false
-clipIntersect = function (n0, n1, n2, n3) {
-	var alphaS,
-		alphaC,
-		result;
-
-	// Returns the intersect node or false
-	result = doesIntersect(n0, n1, n2, n3);
-	
-	if (!result) {
-		return false;
-	} else {
-		var alphaS = norm(n0, result) / norm(n0, n1);
-		var alphaC = norm(n2, result) / norm(n2, n3);
-		
-		return {'alphaS': alphaS, 'alphaC': alphaC, 'x': result.x, 'y': result.y};
-	}
-}
+};
 
 // Function that uses the even/odd rule to determine if a point is inside of a polygon
 // Takes a node object and node array representing a polygon
 // Creates a line vector from the node parameter (x, y) to (0, y)
 // Checks how many times it intersects with each polygon line segment
 // Returns mod 2 of the intersection count: even is external node, odd is internal node
-containmentTest = function (n0, poly) {
+var containmentTest = function(n0, poly) {
 	var i,
-		left = new node(0, n0.y, false, 0.0),
+		left = new Node(0, n0.y, false),
 		type = 0;
 
 	for (i = 0; i < poly.length; i++) {
-		if (clipIntersect(left,
+		if (doesIntersect(left,
 						  n0,
 						  poly[i],
 						  poly[(i === poly.length - 1) ? 0 : i + 1])) {
 			type++;
 		}
 	}
-
-	delete left;
-	return type % 2;
-}
+	return (Boolean)(type % 2);
+};
 
 // First determines the inside/outside value for one vertex of the subject polygon with containmentTest()
 // If it is outside the other polygon then the next intersection point will be marked as entry (1)
 // It it's inside then the next intersect will be an exit (0)
-markEntryPoints = function (subject, clip, flag) {
+var markEntryPoints = function(subject, clip, flag) {
 	var e = containmentTest(subject[0], clip),
 		i;
 
 	// I think this has to do with the other possible boolean operations (http://davis.wpi.edu/~matt/courses/clipping/#two)
 	if (flag){
-		e = 1 - e;
+		e = !e;
 	}
 
 	// Once we found the entry/exit state of that first intersect we just alternate states for the successive intersect nodes
 	for (i = 0; i < clip.length; i++) {
 		if (clip[i].intersect) {
 			clip[i].entry = e;
-			e = 1 - e;
+			e = !e;
 		}
 	}
-
-	return clip;
-}
+};
 
 // Utility clipping function
-// Takes two node arrays representing subject and clip polygons
-// Need to add ability to pass path elements directly and type-check using Raphael.is(obj, "element")
+// Takes two path element objects representing subject and clip polygons
+// Returns the resultant clipped polygons as a Raphael.js element object
 Raphael.fn.clip = function(subject, clip) {
+
+	subject = subject.toNodes();
+	clip = clip.toNodes();
+
 	var subjectLength = subject.length,
 		clipLength = clip.length,
 		subjectIndex,
@@ -212,7 +146,13 @@ Raphael.fn.clip = function(subject, clip) {
 					// That's what this dumb hunk of code is doing
 
 					// Initial next value inside bounds
-					(subjectIndex + 1 === subjectLength) ? i = -subjectIndex : i = 1;
+					if (subjectIndex + 1 === subjectLength) {
+						i = -subjectIndex;
+					}
+					else {
+						i = 1;
+					}
+
 					// Increment and wrap around
 					while (subject[subjectIndex + i].intersect) {
 						i++;
@@ -220,8 +160,15 @@ Raphael.fn.clip = function(subject, clip) {
 							i = -subjectIndex;
 						}
 					}
+
 					// Same steps here for the clip
-					(clipIndex + 1 === clipLength) ? j = -clipIndex : j = 1;
+					if (clipIndex + 1 === clipLength) {
+						j = -clipIndex;
+					}
+					else {
+						j = 1;
+					}
+
 					while (clip[clipIndex + j].intersect) {
 						j++;
 						if (clipIndex + j === clipLength) {
@@ -229,62 +176,51 @@ Raphael.fn.clip = function(subject, clip) {
 						}
 					}
 
-					var result = clipIntersect(subject[subjectIndex],
+					var result = doesIntersect(subject[subjectIndex],
 												subject[subjectIndex + i],
 												clip[clipIndex],
 												clip[clipIndex + j]);
 					
 					if (result)	{
-				 		subject.splice(subjectIndex + 1, 0, new node(result.x, result.y, true, 0.0));
-				 		clip.splice(clipIndex + 1, 0, new node(result.x, result.y, true, 0.0));
-
-				 		subjectLength++;
-				 		clipLength++;
-				 	}
+						subject.splice(subjectIndex + 1, 0, result);
+						clip.splice(clipIndex + 1, 0, result);
+						subjectLength++;
+						clipLength++;
+					}
 				}
 			}	
 		}
 	}
 
 	// Step 2: Mark entry points for both polygons
-	clip = markEntryPoints(subject, clip, true);
-	subject = markEntryPoints(clip, subject, true);	
+	markEntryPoints(subject, clip, true);
+	markEntryPoints(clip, subject, true);	
 
 	// Step 3: Build the clipped polygon
 	// Traverse the subject node list until you get to an entry/exit node, switch to clip node list
 	// Traverse the clip node list until you get to an entry/exit node, switch to subject node list
 	// Stop when you reach the first visited entry/exit node
 	// The variable 'aux' represents the current node we're at
-	var i = 0,
-		aux = subject[i],
+	var aux = subject[0],
 		newPolygon = [],
 		subActive = true,
 		forward = true;
 
-	// Find the first subject entry intersect vertex
-	while (!(aux.intersect && aux.entry > 0)) {
+	i = 0;
+
+	// Start at the first subject entry vertex
+	while (aux.entry != true) {
 		i++;
 		aux = subject[i];
 	}
 	subject[i].visited = true;
-	aux = subject[i];
-	newPolygon.push(aux);
-
-	i++;
-	if ((!subActive && i === clipLength) || (subActive && i === subjectLength)) { 
-		i = 0;
-	}
-
-	aux = subject[i];
-	subject[i].visited = true;
 
 	// Traverse until we hit the first vertex we pushed
-	while (aux.x !== newPolygon[0].x && aux.y !== newPolygon[0].y){
-
+	do {
 		newPolygon.push(aux);
 
-		// If this is a entry/exit node, switch to the neighbour node
-		if (aux.entry !== false) {
+		// If this is an intersect node, switch to the neighbour node
+		if (aux.intersect === true) {
 
 			// Find the neighbour if we're switching from subject to clip
 			if (subActive) {
@@ -295,10 +231,10 @@ Raphael.fn.clip = function(subject, clip) {
 
 				// If it's an entry, go forward
 				// If it's an exit, go backward
-				if (clip[i].entry > 0) {
+				if (clip[i].entry === false) {
 					forward = true;
 				}
-				else if (clip[i].entry === 0) {
+				else {
 					forward = false;
 				}
 			}
@@ -311,13 +247,16 @@ Raphael.fn.clip = function(subject, clip) {
 
 				// If it's an entry, go forward
 				// If it's an exit, go backward
-				if (subject[i].entry > 0) {
+				if (subject[i].entry === true) {
 					forward = true;
 				}
-				else if (subject[i].entry === 0) {
+				else {
 					forward = false;
 				}
 			}
+
+			// We just switched arrays
+			subActive = !subActive;
 
 			// Increment/decrement the counter
 			if (forward) {
@@ -327,9 +266,7 @@ Raphael.fn.clip = function(subject, clip) {
 				i--;
 			}
 
-			subActive = !subActive;
-
-			// Make sure we wrap around the appropriate array
+			// Make sure we wrap around the appropriate array if we need to
 			if (subActive && i === subjectLength) { 
 				i = 0;
 			}
@@ -343,7 +280,7 @@ Raphael.fn.clip = function(subject, clip) {
 				i = clipLength - 1;
 			}
 
-			// Update aux
+			// Update aux to the new node
 			if (subActive) {
 				subject[i].visited = true;
 				aux = subject[i];
@@ -353,8 +290,9 @@ Raphael.fn.clip = function(subject, clip) {
 				aux = clip[i];
 			}
 		}
-		// Not an intersect, traverse ahead in the proper irection
+		// Not an intersect, traverse ahead in the proper direction
 		else {
+
 			// Increment/decrement the counter
 			if (forward) {
 				i++;
@@ -387,14 +325,15 @@ Raphael.fn.clip = function(subject, clip) {
 				aux = clip[i];
 			}	
 		}
-	}
+	} while (aux.x !== newPolygon[0].x && aux.y !== newPolygon[0].y);
 
-	return newPolygon;
-}
+	return paper.path("M" + newPolygon.join("L") + "Z");
+	// End of Raphael.fn.clip()
+};
 
 // Clip the elements of a set
 // Returns a new set containing the new polygons or false if none (no overlap)
-Raphael.st.clip = function() {
+/*Raphael.st.clip = function() {
 	var setSize = this.length,
 		subjectIndex = 0,
 		clipIndex = 1,
@@ -436,7 +375,7 @@ Raphael.st.clip = function() {
 	} else {
 		return false;
 	}
-};
+};*/
 
 // Takes the element's path string and converts it to an array of node objects
 // Ensures that they are in a clockwise order
@@ -448,18 +387,18 @@ Raphael.el.toNodes = function() {
 	pathStringArray.pop(); // Get rid of the Z instruction on the end
 
 	pathStringArray.forEach (function(vertex) {
-		nodeArray.push(new node(vertex[1], vertex[2], false, 0.0));
+		nodeArray.push(new Node(vertex[1], vertex[2], false));
 	});
 
 	// Use the sum of the edges to determine the direction
 	// http://stackoverflow.com/a/1165943/1082395
 	var sum = 0;
 	for (var i = 0; i < nodeArray.length; i++) {
-		sum += (nodeArray[(i + 1 < nodeArray.length) ? i + 1 : 0].x - nodeArray[i].x) * (nodeArray[(i + 1 < nodeArray.length) ? i + 1 : 0].y + nodeArray[i].y)
+		sum += (nodeArray[(i + 1 < nodeArray.length) ? i + 1 : 0].x - nodeArray[i].x) * (nodeArray[(i + 1 < nodeArray.length) ? i + 1 : 0].y + nodeArray[i].y);
 	}
 
 	// If it's CCW, reverse it
-	if (sum < 0) {
+	if (sum > 0) {
 		nodeArray.reverse();
 	}
 
