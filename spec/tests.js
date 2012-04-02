fbe96f26608db46aefbdc43b65f839e6fae8c982
Brandon Evans <brandon@brandonevans.ca>
2012-04-02 15:44:07 -0600

    move tests.js

--- spec/tests.js
+++
@@ -1,7 +0,0 @@
-module("My Module");
-
-test("addTwo", function () {
-    equals(addTwo(0, 0), 0, "Add nothing.");
-    equals(addTwo(1, 2), 3, "Add numbers.");
-    equals(addTwo(-1, -2), -3, "Add negatives.");
-});
